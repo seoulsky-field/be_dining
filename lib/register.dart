1687aa53dae871b64f8ff81e2a8641b398dd1ad4
seoulsky-field <seoulsky.field02@gmail.com>
2024-06-02 19:44:03 +0900

    Feat: add password double checking event and fix email verification bug

--- lib/register.dart
+++ lib/register.dart
@@ -82,6 +82,33 @@ class _RegisterPageState extends State<RegisterPage> {
     }
   }
 
+  String _passwordCheckMessage = "";
+  Color _passwordCheckColor = Colors.red;
+  bool _passwordCheckAlert = false;
+
+  Future<void> _checkPassword(String text) async {
+    final password = passwordController.text;
+    if (password.isEmpty || text.isEmpty) {
+      setState(() {
+        _passwordCheckMessage = '비밀번호를 입력해주세요.';
+        _passwordCheckColor = Colors.red;
+        _passwordCheckAlert = true;
+      });
+    }else if (password != text) {
+      setState(() {
+        _passwordCheckMessage = '입력된 비밀번호가 동일하지 않습니다.';
+        _passwordCheckColor = Colors.red;
+        _passwordCheckAlert = true;
+      });
+    }else {
+      setState(() {
+        _passwordCheckMessage = '비밀번호가 확인되었습니다.';
+        _passwordCheckColor = Colors.blue;
+        _passwordCheckAlert = true;
+      });
+    }
+  }
+
   String _emailDoubleCheckMessage = "";
   Color _emailDoubleCheckColor = Colors.red;
   bool _emailDoubleCheckAlert = false;
@@ -96,7 +123,7 @@ class _RegisterPageState extends State<RegisterPage> {
       });
     }else if (emailNumber == "123456") {
       setState(() {
-        _emailDoubleCheckMessage = '전송된 인증 번호와 드립니다.';
+        _emailDoubleCheckMessage = '전송된 인증 번호와 다릅니다.';
         _emailDoubleCheckColor = Colors.red;
         _emailDoubleCheckAlert = true;
       });
@@ -112,7 +139,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      // appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: SingleChildScrollView(
@@ -126,8 +153,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   width: 75,
                   height: 75,
                   fit: BoxFit.contain
-              ),
-              const SizedBox(height: 20),
+              ), const SizedBox(height: 20),
               SizedBox(
                   height: 20,
                   child: RichText(
@@ -141,8 +167,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   )
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               SizedBox(
                 height: 45,
                 child: TextField(
@@ -180,8 +205,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       )
                   ),
                 ),
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               Visibility(
                   visible: _nicknameCheckAlert,
                   child: SizedBox(
@@ -207,8 +231,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   ),
-              ),
-              const SizedBox(height: 15),
+              ), const SizedBox(height: 15),
               SizedBox(
                   height: 20,
                   child: RichText(
@@ -222,12 +245,12 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   )
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               SizedBox(
                 height: 45,
                 child: TextField(
                   obscureText: true,
+                  controller: passwordController,
                   textAlignVertical: TextAlignVertical.top,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
@@ -249,21 +272,47 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   )
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               SizedBox(
                 height: 45,
                 child: TextField(
                   obscureText: true,
+                  controller: passwordCheckingController,
                   textAlignVertical: TextAlignVertical.top,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(7)
                     ),
                   ),
+                  onChanged: (text) => { _checkPassword(text) },
                 ),
-              ),
-              const SizedBox(height: 30),
+              ), const SizedBox(height: 5),
+              Visibility(
+                visible: _passwordCheckAlert,
+                child: SizedBox(
+                  height: 20,
+                  child: RichText(
+                    textAlign: TextAlign.left,
+                    text: TextSpan(
+                      children: [
+                        WidgetSpan(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                            child: Icon(Icons.info_outline, size: 15, color: _passwordCheckColor),
+                          ),
+                        ),
+                        TextSpan(
+                          text: _passwordCheckMessage,
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: _passwordCheckColor
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ), const SizedBox(height: 30),
               SizedBox(
                   height: 20,
                   child: RichText(
@@ -277,8 +326,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   )
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -315,8 +363,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                 ],
-              ),
-              const SizedBox(height: 10),
+              ), const SizedBox(height: 10),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.white,
@@ -336,8 +383,7 @@ class _RegisterPageState extends State<RegisterPage> {
                         fontWeight: FontWeight.bold
                     )
                 ),
-              ),
-              const SizedBox(height: 5),
+              ), const SizedBox(height: 5),
               Visibility(
                   visible: _emailCheckAlert,
                   child: SizedBox(
@@ -389,6 +435,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     SizedBox(
                       height: 45,
                       child: TextField(
+                        controller: emailCheckingController,
                         textAlignVertical: TextAlignVertical.top,
                         decoration: InputDecoration(
                             border: OutlineInputBorder(
@@ -396,10 +443,6 @@ class _RegisterPageState extends State<RegisterPage> {
                             ),
                             suffixIcon: Padding(
                                 padding: const EdgeInsets.all(7.0),
-                                // child: FittedBox(
-                                //   fit:BoxFit.scaleDown,
-                                //   child,
-                                // ),
                                 child: ElevatedButton(
                                     style: ElevatedButton.styleFrom(
                                       padding: const EdgeInsets.only(top: 0, bottom: 0, right: 0, left: 0),
@@ -493,15 +536,22 @@ class _RegisterPageState extends State<RegisterPage> {
                     const SizedBox(height: 20),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xffbf2142),
-                        surfaceTintColor: Color(0xffbf2142),
+                        backgroundColor: const Color(0xffbf2142),
+                        surfaceTintColor: const Color(0xffbf2142),
                         foregroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(7),
                         ),
                       ),
                       onPressed: () {
-                        Navigator.of(context).push(_createRoute());
+                        if (_nicknameCheckMessage != "사용 가능한 닉네임입니다." ||
+                            _passwordCheckMessage != "비밀번호가 확인되었습니다." ||
+                            _emailCheckMessage != '인증 번호가 전송되었습니다.' ||
+                            _emailDoubleCheckMessage != '인증되었습니다.') {
+                          null;
+                        }else {
+                          Navigator.of(context).push(_createRoute());
+                        }
                       },
                       child: const Text(
                           '가입 완료',
